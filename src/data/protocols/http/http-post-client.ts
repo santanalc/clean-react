1754f1318a5a815d13fd95d4407ec58aaa4088ec
Lucas Santana <lucassantana.santos@protonmail.com>
2021-01-09 12:36:27 -0300

    refactor: adding generic params to HttoPostClient

--- src/data/protocols/http/http-post-client.ts
+++ src/data/protocols/http/http-post-client.ts
@@ -1,9 +1,9 @@
 import { HttpResponse } from "./http-response";
 
-export type HttpPostParams = {
+export type HttpPostParams<T> = {
   url: string;
-  body?: object;
-};
-export interface HttpPostClient {
-  post(param: HttpPostParams): Promise<HttpResponse>;
+  body?: T;
+};  
+export interface HttpPostClient<T, R> { //T será o tipo da body do parâmmentro e R o tipo da body do response
+  post(param: HttpPostParams<T>): Promise<HttpResponse<R>>;
 }
--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -1,6 +1,7 @@
 import { Http2ServerResponse } from "http2";
 import { InvalidCredentialsError } from "../../../domain/errors/invalid-credentials-errors";
 import { UnexpectedError } from "../../../domain/errors/unexpected-error";
+import { AccountModel } from "../../../domain/models/account-model";
 import { AuthenticationParams } from "../../../domain/usecases/authentication";
 import { HttpPostClient } from "../../protocols/http/http-post-client";
 import { HttpStatusCode } from "../../protocols/http/http-response";
@@ -8,7 +9,10 @@ import { HttpStatusCode } from "../../protocols/http/http-response";
 export class RemoteAuthentication {
   constructor(
     private readonly url: string,
-    private readonly httpPostClient: HttpPostClient
+    private readonly httpPostClient: HttpPostClient<
+      AuthenticationParams, //Tipo do parâmetro
+      AccountModel // Tipo do retorno
+    >
   ) {}
 
   async auth(params: AuthenticationParams): Promise<void> {
