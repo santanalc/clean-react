c67b5053cb17766c92d11a558c4f313a53024537
Lucas Santana <lucassantana.santos@protonmail.com>
2021-01-10 11:55:14 -0300

    chore: config sass

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}'],
-  coverageDirectory: 'coverage',
-  testEnvironment: 'jsdom',
+  roots: ["<rootDir>/src"],
+  collectCoverageFrom: ["<rootDir>/src/**/*.{ts,tsx}", "!**/*.d.ts"],
+  coverageDirectory: "coverage",
+  testEnvironment: "jsdom",
   transform: {
-    '.+\\.(ts|tsx)$': 'ts-jest'
-  }
-}
+    ".+\\.(ts|tsx)$": "ts-jest",
+    "\\.scss$": "identity-obj-proxy",
+  },
+};
